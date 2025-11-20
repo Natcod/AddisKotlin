f86ba43bc5b55421f53809072f9aa8b812370cbf
Natcod <natnaelshetaye1@gmail.com>
2025-11-20 16:30:01 +0300

    feat: Implement initial home screen with search, navigation, and core application architecture with a theme package restructure.

---
+++ app/src/main/java/com/app/lyrics/LyricsApp.kt
@@ -0,0 +1,20 @@
+package com.app.lyrics
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class LyricsApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        
+        startKoin {
+            androidLogger()
+            androidContext(this@LyricsApp)
+            modules(
+                appModule
+            )
+        }
+    }
+}
