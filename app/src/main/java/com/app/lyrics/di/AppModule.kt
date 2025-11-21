e01c9e02bc7f01aaa275ffd9cf97c81579fc3b75
Natcod <natnaelshetaye1@gmail.com>
2025-11-21 15:02:06 +0300

    fix: Correct import usage for androidContext in AppModule

--- app/src/main/java/com/app/lyrics/di/AppModule.kt
+++ app/src/main/java/com/app/lyrics/di/AppModule.kt
@@ -5,6 +5,7 @@ import com.app.lyrics.domain.repository.SongRepository
 import com.app.lyrics.presentation.home.HomeViewModel
 import com.app.lyrics.presentation.details.LyricsDetailViewModel
 import com.app.lyrics.presentation.favorites.FavoritesViewModel
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val appModule = module {
@@ -12,7 +13,7 @@ val appModule = module {
     // Database
     single {
         androidx.room.Room.databaseBuilder(
-            org.koin.android.ext.koin.androidContext(),
+            androidContext(),
             com.app.lyrics.data.local.LyricsDatabase::class.java,
             "lyrics_db"
         ).build()
